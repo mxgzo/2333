436d2c042a0a503dea3f937240611572b6d2bdff
mxgzo <770034331@qq.com>
2018-07-18 11:41:51 +0800

    //

--- FlyBird/Image.h
+++ FlyBird/Image.h
@@ -1,7 +1,6 @@
 #pragma once
 #include<stdio.h>
 
-
 struct Color
 {
 	unsigned char r, g, b, a;
--- FlyBird/Scene.cpp
+++ FlyBird/Scene.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include<stdlib.h>
 #include "Scene.h"
 #include"Image.h"
